eb4d4723727c6b50d5d3237417a4734a19a71c93
RyProgrammer <rpwright@ucsd.edu>
2021-10-22 15:18:54 -0700

    Finish expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,6 +3,10 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  var audio = document.querySelector(".hidden");
+  var jsConfetti = new JSConfetti();
+  var partyTime = false;
+
   // Change picture and audio
   const hornSelect = document.getElementById("horn-select");
 
@@ -10,9 +14,10 @@ function init() {
 
   function changeStuff() {
     const img = document.querySelector("img");
-    const audio = document.querySelector(".hidden");
     img.src = "assets/images/" + event.target.value + ".svg";
-    audio.src = "assets/audio" + event.target.value + ".mp3";
+    audio.src = "assets/audio/" + event.target.value + ".mp3";
+
+    partyTime = (event.target.value == "party-horn");
   }
 
   // Change volume
@@ -21,10 +26,24 @@ function init() {
   volumeInput.addEventListener("change", applyVolume);
 
   function applyVolume() {
-    var volumeImg = document.querySelectorAll("img")[1];
+    const volumeImg = document.querySelectorAll("img")[1];
     if(event.target.value >= 67) volumeImg.src = "assets/icons/volume-level-3.svg";
     else if(event.target.value >= 33) volumeImg.src = "assets/icons/volume-level-2.svg";
     else if(event.target.value >= 1) volumeImg.src = "assets/icons/volume-level-1.svg";
     else volumeImg.src = "assets/icons/volume-level-0.svg";
+
+    audio.volume = event.target.value / 100;
+  }
+
+  // Play sound
+  const playButton = document.querySelector("button");
+
+  playButton.addEventListener("click", playSound);
+
+  function playSound() {
+    audio.load();
+    audio.play();
+
+    if(partyTime) jsConfetti.addConfetti();
   }
 }
