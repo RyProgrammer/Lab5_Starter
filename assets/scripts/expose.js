b66613d99b8bf3c0c58fde78b522ee443ec00803
RyProgrammer <rpwright@ucsd.edu>
2021-10-21 17:58:01 -0700

    Update image and audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,18 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Change picture and audio
+  const hornSelect = document.getElementById("horn-select");
+
+  hornSelect.addEventListener('change', changeStuff);
+
+  function changeStuff() {
+    const img = document.querySelector("img");
+    const audio = document.querySelector(".hidden");
+    img.src = "assets/images/" + event.target.value + ".svg";
+    audio.src = "assets/audio" + event.target.value + ".mp3";
+  }
+
+  // Change volume
+  
 }
