f49a4c40e6bfe783b124bb4d862678303dbb3110
RyProgrammer <rpwright@ucsd.edu>
2021-10-22 20:44:00 -0700

    Complete explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,64 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Set up voice options
+  function setVoices() {
+    var voices = speechSynthesis.getVoices();
+    var voiceSelect = document.getElementById("voice-select");
+
+    for(var i = 0; i < voices.length; ++i) {
+      var v = document.createElement("option");
+
+      v.textContent = voices[i].name;
+      v.setAttribute("id", voices[i].name);
+      voiceSelect.appendChild(v);
+    }
+  }
+
+  speechSynthesis.onvoiceschanged = setVoices;
+
+  // Set up text to speech
+  var talkButton = document.querySelector("button");
+  var voiceSelect = document.getElementById("voice-select");
+  var voiceId;
+
+  voiceSelect.addEventListener("change", getId);
+
+  function getId() {
+    voiceId = event.target.value;
+  }
+
+  talkButton.addEventListener("click", sayWords);
+
+  var text;
+
+  function sayWords() {
+    var textSpeak = document.getElementById("text-to-speak");
+    var voices = speechSynthesis.getVoices();
+    var utterance = new SpeechSynthesisUtterance();
+    var smileyFace = document.querySelector("img");
+
+    for(var i = 0; i < voices.length; ++i) {
+      if(voices[i].name == voiceId) {
+        utterance.voice = voices[i];
+        utterance.text = textSpeak.value;
+        break;
+      }
+    }
+
+    speechSynthesis.speak(utterance);
+    smileyFace.src = "assets/images/smiling-open.png";
+
+    utterance.addEventListener("end", noMoreTalk);
+
+    function noMoreTalk() {
+      smileyFace.src = "assets/images/smiling.png";
+    }
+  }
+
+  textSpeak.addEventListener("change", getText);
+  
+  function getText() {
+    text = event.target.value;
+  }
 }
